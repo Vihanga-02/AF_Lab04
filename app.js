fd9429d96f9b1ace0bc1763bd6a0861c6c8b8b1d
Vihanga Edirisinghe <vmedirisinghe02@gmail.com>
2026-02-22 11:38:00 +0530

    create async function
    create Async function that wait for promise function and improve formatting of complete file

--- app.js
+++ app.js
@@ -2,33 +2,33 @@ console.log('Wellcome to My Node.js App!');
 
 const fs = require('fs');
 fs.writeFile('file.txt', 'Hello World!', function (err) {
-if (err) throw err;
-console.log('File saved!');
+    if (err) throw err;
+    console.log('File saved!');
 });
 
 fs.readFile('file.txt', 'utf8', function (err, data) {
-if (err) throw err;
-console.log(data);
+    if (err) throw err;
+    console.log(data);
 });
 
 const http = require('http');
 http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
-res.write('Hello World!');
-res.end();
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Hello World!');
+    res.end();
 }).listen(8080);
 
 const https = require('https');
 https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-let data = '';
-resp.on('data', (chunk) => {
-data += chunk;
-});
-resp.on('end', () => {
-console.log(JSON.parse(data));
-});
+    let data = '';
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+    resp.on('end', () => {
+        console.log(JSON.parse(data));
+    });
 }).on("error", (err) => {
-console.log("Error: " + err.message);
+    console.log("Error: " + err.message);
 });
 
 const myModule = require('./my-module.js');
@@ -47,3 +47,13 @@ myPromise.then((result) => {
 }).catch((error) => {
     console.log(error);
 });
+
+async function myFunction() {
+    try {
+        const result = await myPromise;
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+myFunction();
